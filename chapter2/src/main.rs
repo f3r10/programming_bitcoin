e1fd03aa44381a702c90d042fa06b5bc23e288a5
f3r10 <frledesma@outlook.com>
2023-02-09 20:00:41 -0500

    refactor chapter 2 exercises

--- chapter2/src/main.rs
+++ chapter2/src/main.rs
@@ -1,64 +1,44 @@
-use std::{ops::Add, fmt};
-
-#[derive(Debug, Clone, Copy)]
-struct Point2 {
-    x: i32,
-    y: i32,
-    a: i32,
-    b: i32
-}
+use std::{fmt, ops::Add};
 
 #[derive(Debug, Clone, Copy)]
 enum Point {
     Inf,
-    Point(Point2),
+    Point { x: i32, y: i32, a: i32, b: i32 },
 }
-
+//https://www.desmos.com/calculator/ialhd71we3
 impl Point {
-    pub fn new(x: Option<i32>, y: Option<i32>, a: i32, b: i32) -> Point {
-        if x.is_none() && y.is_none() {
-            return Point::Inf
+    pub fn new(x: i32, y: i32, a: i32, b: i32) -> Point {
+        if y.pow(2) != (x.pow(3) + a * x + b) {
+            panic!("({:?}, {:?}) is not on the curve", x, y);
         }
-         match x {
-             Some(x_l) => {
-                match y {
-                    Some(y_l) => {
-                        if y_l.pow(2) != (x_l.pow(3) + a * x_l + b) {
-                            panic!("({:?}, {:?}) is not on the curve", x, y);
-                        }
-                        let a = Point2 { x: x_l, y: y_l , a, b};
-                        Point::Point(a)
-                    }
-                    None =>
-                        {panic!("invalid inf point")}
-                }
-            },
-             None => {panic!("invalid inf point")},
-         }
+        Point::Point { x, y, a, b }
     }
-    
+
+    pub fn new_inf() -> Point {
+        Point::Inf
+    }
 }
 
-impl PartialEq for Point{
+impl PartialEq for Point {
     fn eq(&self, other: &Self) -> bool {
-        match self {
-            Point::Inf => {
-                match other {
-                    Point::Inf => true,
-                    Point::Point(Point2 { x: x2, y: y2, a: a2, b: b2 }) => {
-                        false 
-                    }
-                }
-            }
-            Point::Point(Point2 { x: x1, y: y1, a: a1, b: b1 }) => {
-                match other {
-                    Point::Inf => false,
-                    Point::Point(Point2 { x: x2, y: y2, a: a2, b: b2 }) => {
-                        x1 == x2 && y1 == y2 && a1 == a2 && b1 == b2 
-                    }
-                }
-
-            }
+        match (self, other) {
+            (Point::Inf, Point::Inf) => true,
+            (Point::Inf, Point::Point {..}) => false,
+            (Point::Point {..}, Point::Inf) => false,
+            (
+                Point::Point {
+                    x: x1,
+                    y: y1,
+                    a: a1,
+                    b: b1,
+                },
+                Point::Point {
+                    x: x2,
+                    y: y2,
+                    a: a2,
+                    b: b2,
+                },
+            ) => x1 == x2 && y1 == y2 && a1 == a2 && b1 == b2,
         }
     }
 
@@ -72,74 +52,117 @@ impl fmt::Display for Point {
         // write!(f, "({:?}, {:?})", self.x.clone().take(), self.y.clone().take())
         match self {
             Point::Inf => {
-                write!(f, "Inf")
+                write!(f, "infinity")
             }
-            Point::Point(Point2 { x: x1, y: y1, a: _a1, b: _b1 }) => {
-                write!(f, "({:?}, {:?})", x1, y1)
+            Point::Point{
+                x: x1,
+                y: y1,
+                a: a1,
+                b: b1,
+            } => {
+                write!(f, "Point({:?}, {:?})_{}_{}", x1, y1, a1, b1)
             }
         }
     }
 }
 
-impl Add for Point{
+impl Add for Point {
     type Output = Self;
 
     fn add(self, other: Self) -> Self::Output {
-        match self {
-            Point::Inf => {
-                return other
-            }
-            Point::Point(Point2 { x: x1, y: y1, a: a1, b: b1 }) => {
-                match other {
-                    Point::Inf => self,
-                    Point::Point(Point2 { x: x2, y: y2, a: a2, b: b2 }) => {
-                        if x1 == x2 && y1 != y2 {
-                            return Point::Inf
-                        }
-                        else if x1 != x2  {
-                            let s = (y2 - y1) / (x2 - x1);
-                            let x = s.pow(2)- x1 - x2;
-                            let y = s * (x1 - x) - y1;
-                            let p = Point2 {x, y, a: a1, b: b1};
-                            return Point::Point(p)
-                            
-                        }
-
-                        else {
-                            let s = (3 * x1.pow(2) + a1)  / (2 * y1);
-                            let x = s.pow(2) - 2 * x1;
-                            let y = s * (x1 - x) - y1;
-                            let p = Point2 {x, y, a: a1, b: b1};
-                            return Point::Point(p)
-
-                        }
-                        // if a1 !=  a2 || b1 != b2 {
-                        //     panic!("Points {}, {} are not on the same curve", self, other);
-                        // }
-                    }
+        match (self, other) {
+            (Point::Inf, Point::Inf) => Point::Inf,
+            (Point::Inf, p @ Point::Point{..}) => p,
+            (p @ Point::Point{..}, Point::Inf) => p,
+            (
+                p1 @ Point::Point{
+                x: x1,
+                y: y1,
+                a: a1,
+                b: b1,
+            },
+                p2 @ Point::Point{
+                x: x2,
+                y: y2,
+                a: a2,
+                b: b2,
+            },
+            ) => {
+                if a1 != a2 || b1 != b2 {
+                    panic!("Points {}, {} are not on the same curve", p1, p2)
                 }
-
+                // Additive inverses points
+                if x1 == x2 && y1 != y2 {
+                    return Point::Inf;
+                } else if x1 != x2 {
+                    let s = (y2 - y1) / (x2 - x1);
+                    let x = s.pow(2) - x1 - x2;
+                    let y = s * (x1 - x) - y1;
+                    return Point::Point{ x, y, a: a1, b: b1 };
+                } else if p1 == p2 && y1 == 0 {
+                    Point::Inf
+                } else if p1 == p2 {
+                    let s = (3 * x1.pow(2) + a1) / (2 * y1);
+                    let x = s.pow(2) - 2 * x1;
+                    let y = s * (x1 - x) - y1;
+                    return Point::Point{ x, y, a: a1, b: b1 };
+                } else {
+                    panic!("no more cases")
+                }
             }
         }
     }
 }
 
-
 fn main() {
-    let p11 = Point::new(Some(-1), Some(-1), 5, 7);
-    // let p2 = Point::new(-1, -2, 5, 7);
-    // let p2 = Point::new(2, 4, 5, 7);
-    let p3 = Point::new(Some(-1), Some(-1), 5, 7);
-    let p4 = Point::new(Some(18), Some(77), 5, 7);
-    let inf = Point::new(None, None, 5, 7);
-    println!("{:?}", p11);
-    println!("(-1, -1) different from (18, 77) -> {}", p3 != p4);
-    // let p5 = Point::new(5, 7, 5, 7);
-    let p1 = Point::new(Some(2), Some(5), 5, 7);
-    let p2 = Point::new(Some(-1), Some(-1), 5, 7);
-    let p3 = p1 + p2;
-    println!("{:?}", p3);
-    let p4 = p11 + p11;
-    println!("{:?}", p4);
+    let p = Point::new(-1, -1, 5, 7);
+    let inf = Point::new_inf();
+    let res = p + inf;
+    print!("{}", res)
+}
 
+#[cfg(test)]
+mod point_test {
+    use crate::Point;
+
+    #[test]
+    fn point_inside_curve() {
+        let _p = Point::new(-1, -1, 5, 7);
+        let _p = Point::new(18, 77, 5, 7);
+    }
+    #[test]
+    #[should_panic]
+    fn point_outside_curve() {
+        let _p = Point::new(-1, -2, 5, 7);
+        let _p = Point::new(2, 4, 5, 7);
+        let _p = Point::new(5, 7, 5, 7);
+    }
+    #[test]
+    fn add_inf() {
+        let p1 = Point::new(-1, -1, 5, 7);
+        let p2 = Point::new(-1, 1, 5, 7);
+        let inf = Point::new_inf();
+        assert_eq!(p1 + inf, p1);
+        assert_eq!(inf + p2, p2);
+        assert_eq!(p1 + p2, inf);
+    }
+    #[test]
+    fn add_different_x() {
+        let p1 = Point::new(3, 7, 5, 7);
+        let p2 = Point::new(-1, -1, 5, 7);
+        let p3 = Point::new(2, -5, 5, 7);
+        assert_eq!(p1 + p2, p3);
+    }
+    #[test]
+    fn add_same_point() {
+        let p = Point::new(-1, -1, 5, 7);
+        let p2 = Point::new(18, 77, 5, 7);
+        assert_eq!(p + p, p2);
+    }
+    #[test]
+    fn add_same_x_different_y() {
+        let p1 = Point::new(-1, 1, 5, 7);
+        let p2 = Point::new(-1, -1, 5, 7);
+        assert_eq!(p1 + p2, Point::new_inf());
+    }
 }

168772e7ca2c8f63a6e203995ded23a61bbca781
f3r10 <frledesma@outlook.com>
2023-02-13 19:22:54 -0500

    Complete exercise 4 and 5

--- chapter3/src/bin/bitcoin-clone.rs
+++ chapter3/src/bin/bitcoin-clone.rs
@@ -15,8 +15,84 @@ fn main() {
     let x2 = FiniteField::new(60, prime);
     let y2 = FiniteField::new(139, prime);
     let p1 = PointWrapper::new(x1, y1, a.clone(), b.clone());
-    let p2 = PointWrapper::new(x2, y2, a, b);
-    println!("{}", p1 + p2)
+    let p2 = PointWrapper::new(x2, y2, a.clone(), b.clone());
+    println!("{}", p1 + p2);
+    println!("=====exercise 4=======");
+    let p3 = PointWrapper::new(
+        FiniteField::new(192, prime),
+        FiniteField::new(105, prime),
+        a.clone(),
+        b.clone(),
+    );
+    println!("{}", p3.clone() + p3);
+    let p4 = PointWrapper::new(
+        FiniteField::new(143, prime),
+        FiniteField::new(98, prime),
+        a.clone(),
+        b.clone(),
+    );
+    println!("{}", p4.clone() + p4);
+    let p5 = PointWrapper::new(
+        FiniteField::new(47, prime),
+        FiniteField::new(71, prime),
+        a.clone(),
+        b.clone(),
+    );
+    println!("{}", p5.clone() + p5.clone());
+    println!("{}", p5.clone() + p5.clone() + p5.clone() + p5.clone());
+    println!(
+        "{}",
+        p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+    );
+    println!(
+        "{}",
+        p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+            + p5.clone()
+    );
+    println!("=====exercise 5=======");
+    let p5 = PointWrapper::new(
+        FiniteField::new(15, prime),
+        FiniteField::new(86, prime),
+        a.clone(),
+        b.clone(),
+    );
+    let mut count = 0;
+    let inf = PointWrapper::new_inf();
+    let mut inc = PointWrapper::new_inf();
+    loop {
+        inc = inc + p5.clone();
+        count += 1;
+        if inc == inf {
+            break;
+        }
+    }
+    println!("The order of the group generate by (15, 86) is {}", count);
 }
 
 // y^2 = x^3 + y

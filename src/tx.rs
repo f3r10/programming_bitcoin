51d0e132e061c9af317fdb5ee5eb9a3a143d51d9
f3r10 <frledesma@outlook.com>
2023-04-09 11:36:23 -0500

    Add coinbase_height method

--- src/tx.rs
+++ src/tx.rs
@@ -244,6 +244,19 @@ impl Tx {
         }
         true
     }
+
+    pub fn coinbase_height(&self) -> Option<BigInt> {
+        if !self.is_coinbase() {
+            return None
+        }
+        let first_tx = &self.tx_ins[0];
+        first_tx.script_sig.as_ref().map(|script_sig| {
+            match &script_sig.cmds[0] {
+                Command::Element(elm) => utils::little_endian_to_int(&elm),
+                Command::Operation(_) => panic!("Invalid coinbase first cmd"),
+            }
+        })
+    }
 }
 
 impl Display for Tx {
@@ -643,5 +656,18 @@ mod tx_tests {
         let mut stream = Cursor::new(raw_tx);
         let tx = Tx::parse(&mut stream, false);
         assert!(tx.is_coinbase())
+    }
+
+    #[test]
+    fn test_coinbase_height() {
+        let raw_tx = hex::decode("01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff5e03d71b07254d696e656420627920416e74506f6f6c20626a31312f4542312f4144362f43205914293101fabe6d6d678e2c8c34afc36896e7d9402824ed38e856676ee94bfdb0c6c4bcd8b2e5666a0400000000000000c7270000a5e00e00ffffffff01faf20b58000000001976a914338c84849423992471bffb1a54a8d9b1d69dc28a88ac00000000").unwrap();
+        let mut stream = Cursor::new(raw_tx);
+        let tx = Tx::parse(&mut stream, false);
+        assert_eq!(tx.coinbase_height(), Some(BigInt::from(465879)));
+
+        let raw_tx = hex::decode("0100000001813f79011acb80925dfe69b3def355fe914bd1d96a3f5f71bf8303c6a989c7d1000000006b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278afeffffff02a135ef01000000001976a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141c4bc762dd5423e332166702cb75f40df79fea1288ac19430600").unwrap();
+        let mut stream = Cursor::new(raw_tx);
+        let tx = Tx::parse(&mut stream, false);
+        assert_eq!(tx.coinbase_height(), None)
     }
 }
